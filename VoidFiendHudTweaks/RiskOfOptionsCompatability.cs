4b0f964379ed881424ef377dc2c2b254140ea114
treacherousfiend <treacherousfiend@proton.me>
2023-11-23 01:57:53 -0700

    Big changes
    -Move from IL hooks to regular hooks (for easy of programming, and cause idk how to handle settings changes in IL hooks)
    -Add bar extension which shows how much percentage suppress will give/use
    -add optional Risk of Options support
    
    TODO:
    -Add delta notifications (i.e. show +5%, -20%, when you heal or crit)
    -Add a timer for corruption
    -hide corrupted suppress delta when you have no charges of it.
    -maybe pick better default colors?
    -maybe look into just straight up skipping some code if an option is disabled?

---
+++ VoidFiendHudTweaks/RiskOfOptionsCompatability.cs
@@ -0,0 +1,95 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using System;
+
+namespace VoidFiendHudTweaks
+{
+	public static class RiskOfOptionsCompatibility
+	{
+		private static bool? _enabled;
+		public static bool Enabled
+		{
+			get
+			{
+				if (_enabled == null)
+				{
+					_enabled = BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("com.rune580.riskofoptions");
+				}
+				return (bool)_enabled;
+			}
+		}
+
+		public static void AddCheckBoxOption(ConfigEntry<bool> newEntry)
+		{
+			RiskOfOptions.ModSettingsManager.AddOption(new RiskOfOptions.Options.CheckBoxOption(newEntry));
+		}
+
+		public static void AddCheckBoxOption(ConfigEntry<bool> newEntry, bool restartRequired = false)
+		{
+			RiskOfOptions.ModSettingsManager.AddOption(new RiskOfOptions.Options.CheckBoxOption(newEntry, restartRequired));
+		}
+
+		public static void AddCheckBoxOption(ConfigEntry<bool> newEntry, string inputName = null, string inputDescription = null, string inputCategory = null, bool inputRestartRequired = false, Delegate inputCheckIfDisabled = null )
+		{
+			var OptionConfig = new RiskOfOptions.OptionConfigs.CheckBoxConfig()
+			{
+				name = inputName,
+				description = inputDescription,
+				category = inputCategory,
+				restartRequired = inputRestartRequired,
+				// I have no clue how this works, so I hope that it does in fact work.
+				checkIfDisabled = (RiskOfOptions.OptionConfigs.BaseOptionConfig.IsDisabledDelegate)inputCheckIfDisabled
+			};
+			RiskOfOptions.ModSettingsManager.AddOption(new RiskOfOptions.Options.CheckBoxOption(newEntry, OptionConfig));
+		}
+
+		public static void AddColorOption(ConfigEntry<UnityEngine.Color> newEntry)
+		{
+			RiskOfOptions.ModSettingsManager.AddOption(new RiskOfOptions.Options.ColorOption(newEntry));
+		}
+
+        public static void AddColorOption(ConfigEntry<UnityEngine.Color> newEntry, bool restartRequired = false)
+        {
+            RiskOfOptions.ModSettingsManager.AddOption(new RiskOfOptions.Options.ColorOption(newEntry, restartRequired));
+        }
+
+        public static void AddColorOption(ConfigEntry<UnityEngine.Color> newEntry, string inputName = null, string inputDescription = null, string inputCategory = null, bool inputRestartRequired = false, Delegate inputCheckIfDisabled = null)
+        {
+            var OptionConfig = new RiskOfOptions.OptionConfigs.ColorOptionConfig()
+            {
+                name = inputName,
+                description = inputDescription,
+                category = inputCategory,
+                restartRequired = inputRestartRequired,
+                // I have no clue how this works, so I hope that it does in fact work.
+                checkIfDisabled = (RiskOfOptions.OptionConfigs.BaseOptionConfig.IsDisabledDelegate)inputCheckIfDisabled
+            };
+            RiskOfOptions.ModSettingsManager.AddOption(new RiskOfOptions.Options.ColorOption(newEntry, OptionConfig));
+        }
+
+        public static void AddColorOption(ConfigEntry<UnityEngine.Color> newEntry, Func<bool> inputCheckIfDisabled = null)
+        {
+            var OptionConfig = new RiskOfOptions.OptionConfigs.ColorOptionConfig()
+            {
+                // I have no clue how this works, so I hope that it does in fact work.
+                checkIfDisabled = new RiskOfOptions.OptionConfigs.BaseOptionConfig.IsDisabledDelegate(inputCheckIfDisabled)
+            };
+            RiskOfOptions.ModSettingsManager.AddOption(new RiskOfOptions.Options.ColorOption(newEntry, OptionConfig));
+        }
+
+        //public static RiskOfOptions.Options.CheckBoxOption CreateCheckBoxOption(ConfigEntry<bool> newEntry)
+        //{
+        //	return new RiskOfOptions.Options.CheckBoxOption(newEntry);
+        //}
+
+        //public static void InvokeAddOption(RiskOfOptions.Options.BaseOption option)
+        //{
+        //	RiskOfOptions.ModSettingsManager.AddOption(option);
+        //}
+
+        public static void InvokeSetModDescription(string description)
+		{
+			RiskOfOptions.ModSettingsManager.SetModDescription(description);
+		}
+	}
+}
--- VoidFiendHudTweaks/VoidFiendHudTweaks.cs
+++ VoidFiendHudTweaks/VoidFiendHudTweaks.cs
@@ -1,29 +1,23 @@
 using BepInEx;
 using BepInEx.Configuration;
-using R2API;
 using RoR2;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
-using Mono.Cecil.Cil;
-using MonoMod.Cil;
-using System.Collections.Generic;
-using IL.RoR2.UI;
-using On.RoR2.UI;
-using System;
-using RiskOfOptions;
-using RiskOfOptions.Options;
+using RoR2.HudOverlay;
+using RoR2.UI;
+using UnityEngine.UI;
+using TMPro;
+using System.Text;
 
 namespace VoidFiendHudTweaks
 {
 	// R2API dependency
-    [BepInDependency(R2API.R2API.PluginGUID)]
+	[BepInDependency(R2API.R2API.PluginGUID)]
 
-    // Add Risk of Options dependency
-    [BepInDependency("com.rune580.riskofoptions")]
+	// Add Risk of Options dependency
+	[BepInDependency("com.rune580.riskofoptions", BepInDependency.DependencyFlags.SoftDependency)]
 
-
-    // This attribute is required, and lists metadata for your plugin.
-    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+	// This attribute is required, and lists metadata for your plugin.
+	[BepInPlugin(PluginGUID, PluginName, PluginVersion)]
 
 	// This is the main declaration of our plugin class.
 	// BepInEx searches for all classes inheriting from BaseUnityPlugin to initialize on startup.
@@ -32,110 +26,253 @@ namespace VoidFiendHudTweaks
 	// More information in the Unity Docs: https://docs.unity3d.com/ScriptReference/MonoBehaviour.html
 	public class VoidFiendHudTweaks : BaseUnityPlugin
 	{
-		// The Plugin GUID should be a unique ID for this plugin,
-		// which is human readable (as it is used in places like the config).
-		// If we see this PluginGUID as it is on thunderstore,
-		// we will deprecate this mod.
-		// Change the PluginAuthor and the PluginName !
 		public const string PluginGUID = PluginAuthor + "." + PluginName;
 		public const string PluginAuthor = "fiend";
 		public const string PluginName = "Void-Fiend-UI-Tweak";
 		public const string PluginVersion = "1.0.0";
 		
 		public static ConfigEntry<bool> ConfigCorruptionPercentageTweak { get; set; }
+		
 		// TODO: Implement these features
-        public static ConfigEntry<bool> ConfigCorruptionTimer { get; set; }
-        public static ConfigEntry<bool> ConfigCorruptionDelta { get; set; }
+		public static ConfigEntry<bool> ConfigCorruptionTimer { get; set; }
+		
+		public static ConfigEntry<bool> ConfigCorruptionDelta { get; set; }
+		
+		public static ConfigEntry<bool> ConfigSuppressHealDelta { get; set; }
+		public static ConfigEntry<Color> ConfigSuppressHealDeltaColor { get; set; }
+		
+		public static ConfigEntry<bool> ConfigSuppressCorruptDelta { get; set; }
+		public static ConfigEntry<Color> ConfigSuppressCorruptDeltaColor { get; set; }
 
+		public static Image SuppressHealDeltaImageObject = null;
+		public static Image SuppressCorruptDeltaImageObject = null;
 
+		public void OnEnable()
+		{
+			On.RoR2.VoidSurvivorController.OnEnable += VoidSurvivorController_OnEnable;
+			On.RoR2.VoidSurvivorController.OnDisable += VoidSurvivorController_OnDisable;
+			On.RoR2.VoidSurvivorController.UpdateUI += CustomUpdateUI;
+			On.RoR2.VoidSurvivorController.OnOverlayInstanceAdded += VoidSurvivorController_OnOverlayInstanceAdded;
+		}
 
-        // The Awake() method is run at the very start when the game is initialized.
-        public void Awake()
+		public void OnDisable()
+		{
+			On.RoR2.VoidSurvivorController.OnEnable -= VoidSurvivorController_OnEnable;
+			On.RoR2.VoidSurvivorController.OnDisable -= VoidSurvivorController_OnDisable;
+			On.RoR2.VoidSurvivorController.UpdateUI -= CustomUpdateUI;
+			On.RoR2.VoidSurvivorController.OnOverlayInstanceAdded -= VoidSurvivorController_OnOverlayInstanceAdded;
+		}
+
+		private void CustomUpdateUI(On.RoR2.VoidSurvivorController.orig_UpdateUI orig, VoidSurvivorController self)
+		{
+			orig(self);
+
+			ImageFillController baseFillUi = null;
+
+			// For now, just pick the last one in the list.
+			// In normal gameplay this should only have 1 value, but mods may change this, so for compat this should be more indepth. 
+			foreach (ImageFillController fillUi in self.fillUiList)
+			{
+				baseFillUi = fillUi;
+			}
+
+			// Workaround because UpdateUI is run once before OnOverlayInstanceAdded
+			if (self.fillUiList.Count > 0)
+			{
+				float CorruptionPercentage = self.corruption / self.maxCorruption;
+				float SuppressHealDelta = 0f;
+
+				if (self.isCorrupted == false)
+				{
+					if (ConfigSuppressHealDelta.Value)
+					{
+						if (CorruptionPercentage >= 0.25f && CorruptionPercentage < 1f)
+						{
+							SuppressHealDelta = CorruptionPercentage;
+							CorruptionPercentage = Mathf.Clamp(CorruptionPercentage, self.minimumCorruption, CorruptionPercentage - 0.25f);
+						}
+						else
+						{
+							SuppressHealDeltaImageObject.fillAmount = 0f;
+						}
+					}
+
+					baseFillUi.SetTValue(CorruptionPercentage);
+					SuppressHealDeltaImageObject.fillAmount = SuppressHealDelta;
+					SuppressCorruptDeltaImageObject.fillAmount = 0f;
+
+				}
+				else
+				{
+					float CorruptionDelta = 0f;
+
+					if (ConfigCorruptionPercentageTweak.Value)
+					{
+						// In order to get the original 0-100%, we have to convert it by assuming that the current corruption percentage is
+						// (corruption - minCorruption) / (maxCorruption - minCorruption)
+						// While technically, the game always uses 0-100%, if for example, minCorruption was 30%, then its technically a 0-70% scale.
+						// So from there we can get the percentage along that 0-70% scale and that is our 0-100% scale percentage.
+						// Example:
+						// corruption = 90, minCorruption = 30, maxCorruption = 100
+						// (corruption - minCorruption) [60] / (maxCorruption - minCorruption) [70] = 0.8571428571
+						CorruptionPercentage = (self.corruption - self.minimumCorruption) / (self.maxCorruption - self.minimumCorruption);
+
+						StringBuilder stringBuilder = HG.StringBuilderPool.RentStringBuilder();
+						stringBuilder.AppendInt(Mathf.FloorToInt(CorruptionPercentage * 100), 1u, 3u).Append("%");
+						self.uiCorruptionText.GetComponentInChildren<TextMeshProUGUI>().SetText(stringBuilder);
+						HG.StringBuilderPool.ReturnStringBuilder(stringBuilder);
+
+						if (ConfigSuppressCorruptDelta.Value)
+						{
+							CorruptionDelta = Mathf.Min(1f, CorruptionPercentage + (25f / (self.maxCorruption - self.minimumCorruption)));
+						}
+						
+					}
+					else if(ConfigSuppressCorruptDelta.Value)
+					{
+						CorruptionDelta = Mathf.Min(1f, CorruptionPercentage + 0.25f);
+					};
+
+					baseFillUi.SetTValue(CorruptionPercentage);
+					SuppressHealDeltaImageObject.fillAmount = 0f;
+					SuppressCorruptDeltaImageObject.fillAmount = CorruptionDelta;
+				}
+			}
+		}
+
+		private void RecursiveFindGameObjectChildren(Transform gameObjectTransform, int indentCount)
+		{
+			foreach (Transform childTransform in gameObjectTransform)
+			{
+				int i;
+				string DebugString = "";
+				for (i = 0; i <= indentCount; i++)
+				{
+					DebugString += "\t";
+				}
+
+				Debug.Log(DebugString + childTransform.gameObject);
+
+				RecursiveFindGameObjectChildren(childTransform.transform, i + 1);
+			}
+		}
+
+		private void VoidSurvivorController_OnOverlayInstanceAdded(On.RoR2.VoidSurvivorController.orig_OnOverlayInstanceAdded orig, VoidSurvivorController self, OverlayController controller, GameObject instance)
+		{
+			GameObject FillObject = null;
+
+			// Someone please kill me for this code
+			foreach (Transform childTransform in instance.transform)
+			{
+				if (childTransform.gameObject.name == "FillRoot")
+				{
+					foreach (Transform childTransform2 in childTransform)
+					{
+						if (childTransform2.gameObject.name == "Fill")
+						{
+							foreach (Transform childTransform3 in childTransform2)
+							{
+								if (childTransform3.gameObject.name == "Fill")
+								{
+									FillObject = childTransform3.gameObject;
+								}
+							}
+						}
+					}
+				}
+			}
+			if (FillObject != null)
+			{
+				GameObject SuppressHealDeltaObject = Instantiate(FillObject);
+				SuppressHealDeltaObject.name = "FillSuppressHealDelta";
+				SuppressHealDeltaImageObject = SuppressHealDeltaObject.GetComponent<UnityEngine.UI.Image>();
+				SuppressHealDeltaImageObject.color = ConfigSuppressHealDeltaColor.Value;
+				SuppressHealDeltaObject.transform.SetParent(FillObject.transform.parent, false);
+				SuppressHealDeltaObject.transform.SetSiblingIndex(1);
+
+				GameObject SuppressCorruptDeltaObject = Instantiate(FillObject);
+				SuppressCorruptDeltaObject.name = "FillSuppressCorruptDelta";
+				SuppressCorruptDeltaImageObject = SuppressCorruptDeltaObject.GetComponent<UnityEngine.UI.Image>();
+				SuppressCorruptDeltaImageObject.color = ConfigSuppressCorruptDeltaColor.Value;
+				SuppressCorruptDeltaObject.transform.SetParent(FillObject.transform.parent, false);
+				SuppressCorruptDeltaObject.transform.SetSiblingIndex(2);
+			}
+			
+
+			orig(self, controller, instance);
+		}
+
+		private void VoidSurvivorController_OnEnable(On.RoR2.VoidSurvivorController.orig_OnEnable orig, VoidSurvivorController self)
+		{
+			orig(self);
+		}
+
+		private void VoidSurvivorController_OnDisable(On.RoR2.VoidSurvivorController.orig_OnDisable orig, VoidSurvivorController self)
+		{
+			orig(self);
+		}
+
+		// The Awake() method is run at the very start when the game is initialized.
+		public void Awake()
 		{
 			// Init our logging class so that we can properly log for debugging
 			Log.Init(Logger);
 
-			ConfigCorruptionPercentageTweak = Config.Bind<bool>(
+			ConfigCorruptionPercentageTweak = Config.Bind(
 				"UI Tweaks",
 				"Always decrement corruption to 0%",
 				true,
 				"When in corrupted mode, always decrement the hud percentage from 100% to 0% regardless of increased minimum." +
-				"\nDirect changes to the percentage, such as the \"Corrupted Supress\" ability will appear to change the percentage more than they actually do!" +
-				"\nCurrently requires a game restart to change"
+				"\n\nDirect changes to the percentage, such as the \"Corrupted Suppress\" ability will appear to change the percentage more than they actually do!" +
+				"\nI recommend using \"Show Suppress Corruption Delta\" with this option to make these changes a bit clearer" +
+				"\n\nDefault: True"
 				);
-			ModSettingsManager.AddOption(new CheckBoxOption(ConfigCorruptionPercentageTweak));
 
-			ModSettingsManager.SetModDescription("Tweak various things about the Void Fiend corruption hud to fit your liking");
+			ConfigSuppressHealDelta = Config.Bind(
+				"UI Tweaks",
+				"Show Suppress Heal Delta",
+				false,
+				"Show on the corruption meter how much will be removed by Suppress" +
+				"\n\nDefault: False"
+				);
 
+			ConfigSuppressHealDeltaColor = Config.Bind(
+				"UI Tweaks",
+				"Suppress Heal Delta Color",
+				new Color(0.58984375f, 0.265625f, 0.375f, 1f),
+				"The color that will be used if \"Show Suppress Heal Delta\" is enabled" +
+				"\n\nDefault: 964460FF"
+				);
 
-			// (fiend) new code, just adding on top for now.
-			IL.RoR2.VoidSurvivorController.UpdateUI += (il) =>
+			ConfigSuppressCorruptDelta = Config.Bind(
+				"UI Tweaks",
+				"Show Suppress Corruption Delta",
+				false,
+				"Show on the corruption meter how much will be added by Corrupted Suppress" +
+				"\n\nDefault: False"
+				);
+
+			ConfigSuppressCorruptDeltaColor = Config.Bind(
+				"UI Tweaks",
+				"Suppress Corruption Delta Color",
+				new Color(0.6953125f, 0.15234375f, 0.1796875f, 1f),
+				"The color that will be used if \"Show Suppress Corruption Delta\" is enabled" +
+				"\n\nDefault: B2272EFF"
+				);
+
+			// Only do this is RiskOfOptions is installed!
+			if (RiskOfOptionsCompatibility.Enabled)
 			{
-				if (ConfigCorruptionPercentageTweak.Value == true)
-				{
-					ILCursor cursor = new ILCursor(il);
-					cursor.GotoNext(
-						x => x.MatchLdarg(0)
-					//x => x.MatchLdfld<RoR2.UI.ImageFillController>("fillUi in fillUiList"),
-					//x => x.MatchCallvirt<RoR2.UI.ImageFillController>("GetEnumerator"),
-					//x => x.MatchStloc(0)
-					);
-					cursor.Index += 11;
+				RiskOfOptionsCompatibility.AddCheckBoxOption(ConfigCorruptionPercentageTweak);
+				
+				RiskOfOptionsCompatibility.AddCheckBoxOption(ConfigSuppressHealDelta);
+				RiskOfOptionsCompatibility.AddColorOption(ConfigSuppressHealDeltaColor, delegate () { return !ConfigSuppressHealDelta.Value; } );
 
-					cursor.Emit(OpCodes.Ldarg_0);
+				RiskOfOptionsCompatibility.AddCheckBoxOption(ConfigSuppressCorruptDelta);
+				RiskOfOptionsCompatibility.AddColorOption(ConfigSuppressCorruptDeltaColor, delegate () { return !ConfigSuppressCorruptDelta.Value; });
 
-					// In order to get the original 0-100%, we have to convert it by assuming that the current corruption percentage is
-					// (corruption - minCorruption) / (maxCorruption - minCorruption)
-					// While technically, the game always uses 0-100%, if for example, minCorruption was 30%, then its technically a 0-70% scale.
-					// So from there we can get the percentage along that 0-70% scale and that is our 0-100% scale percentage.
-					// Example:
-					// corruption = 90, minCorruption = 30, maxCorruption = 100
-					// (corruption - minCorruption) [60] / (maxCorruption - minCorruption) [70] = 0.8571428571
-					cursor.EmitDelegate<Func<float, float, VoidSurvivorController, float>>((corruption, maxCorruption, currController) =>
-					{
-						if (currController.isCorrupted == true)
-						{
-							var minCorruption = currController.minimumCorruption;
-							return (corruption - minCorruption) / (maxCorruption - minCorruption);
-						}
-						else
-						{
-							return corruption / maxCorruption;
-						}
-					});
-					cursor.Next.Operand = null;
-					cursor.Next.OpCode = OpCodes.Nop;
-
-					// Maybe a bit too loose?
-					cursor.GotoNext(
-						//x => x.MatchCallOrCallvirt("HG.StringBuilderPool","RentStringBuilder"),
-						x => x.MatchStloc(1),
-						x => x.MatchLdloc(1),
-						x => x.MatchLdarg(0)
-						//x => x.MatchCallOrCallvirt<VoidSurvivorController>("get_controller")
-						//x => x.MatchCallOrCallvirt<Mathf>("FloorToInt")
-					);
-
-					cursor.Index += 4;
-
-					cursor.Emit(OpCodes.Ldarg_0);
-					cursor.EmitDelegate<Func<float, VoidSurvivorController, float>>((corruption, currController) =>
-					{
-						if (currController.isCorrupted == true)
-						{
-							var minCorruption = currController.minimumCorruption;
-							return ((corruption - minCorruption) / (currController.maxCorruption - minCorruption)) * 100;
-						}
-						else
-						{
-							return corruption;
-						}
-					});
-#if DEBUG
-					Debug.Log(il.ToString());
-#endif
-				}
-			};
+				RiskOfOptionsCompatibility.InvokeSetModDescription("Tweak various things about the Void Fiend corruption hud to fit your liking");
+			}
 		}
 	}
 }
