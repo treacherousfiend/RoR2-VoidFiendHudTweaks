e4d5c8d7a55585bdaa4584934f22a8e075bd80c2
treacherousfiend <treacherousfiend@proton.me>
2023-11-29 02:53:11 -0700

    fix file using mixed spacing
    tabs 4 lyfe

--- VoidFiendHudTweaks/RiskOfOptionsCompatability.cs
+++ VoidFiendHudTweaks/RiskOfOptionsCompatability.cs
@@ -47,46 +47,46 @@ namespace VoidFiendHudTweaks
 			RiskOfOptions.ModSettingsManager.AddOption(new RiskOfOptions.Options.ColorOption(newEntry));
 		}
 
-        public static void AddColorOption(ConfigEntry<UnityEngine.Color> newEntry, bool restartRequired = false)
-        {
-            RiskOfOptions.ModSettingsManager.AddOption(new RiskOfOptions.Options.ColorOption(newEntry, restartRequired));
-        }
+		public static void AddColorOption(ConfigEntry<UnityEngine.Color> newEntry, bool restartRequired = false)
+		{
+			RiskOfOptions.ModSettingsManager.AddOption(new RiskOfOptions.Options.ColorOption(newEntry, restartRequired));
+		}
 
-        public static void AddColorOption(ConfigEntry<UnityEngine.Color> newEntry, string inputName = null, string inputDescription = null, string inputCategory = null, bool inputRestartRequired = false, Delegate inputCheckIfDisabled = null)
-        {
-            var OptionConfig = new RiskOfOptions.OptionConfigs.ColorOptionConfig()
-            {
-                name = inputName,
-                description = inputDescription,
-                category = inputCategory,
-                restartRequired = inputRestartRequired,
-                // I have no clue how this works, so I hope that it does in fact work.
-                checkIfDisabled = (RiskOfOptions.OptionConfigs.BaseOptionConfig.IsDisabledDelegate)inputCheckIfDisabled
-            };
-            RiskOfOptions.ModSettingsManager.AddOption(new RiskOfOptions.Options.ColorOption(newEntry, OptionConfig));
-        }
+		public static void AddColorOption(ConfigEntry<UnityEngine.Color> newEntry, string inputName = null, string inputDescription = null, string inputCategory = null, bool inputRestartRequired = false, Delegate inputCheckIfDisabled = null)
+		{
+			var OptionConfig = new RiskOfOptions.OptionConfigs.ColorOptionConfig()
+			{
+				name = inputName,
+				description = inputDescription,
+				category = inputCategory,
+				restartRequired = inputRestartRequired,
+				// I have no clue how this works, so I hope that it does in fact work.
+				checkIfDisabled = (RiskOfOptions.OptionConfigs.BaseOptionConfig.IsDisabledDelegate)inputCheckIfDisabled
+			};
+			RiskOfOptions.ModSettingsManager.AddOption(new RiskOfOptions.Options.ColorOption(newEntry, OptionConfig));
+		}
 
-        public static void AddColorOption(ConfigEntry<UnityEngine.Color> newEntry, Func<bool> inputCheckIfDisabled = null)
-        {
-            var OptionConfig = new RiskOfOptions.OptionConfigs.ColorOptionConfig()
-            {
-                // I have no clue how this works, so I hope that it does in fact work.
-                checkIfDisabled = new RiskOfOptions.OptionConfigs.BaseOptionConfig.IsDisabledDelegate(inputCheckIfDisabled)
-            };
-            RiskOfOptions.ModSettingsManager.AddOption(new RiskOfOptions.Options.ColorOption(newEntry, OptionConfig));
-        }
+		public static void AddColorOption(ConfigEntry<UnityEngine.Color> newEntry, Func<bool> inputCheckIfDisabled = null)
+		{
+			var OptionConfig = new RiskOfOptions.OptionConfigs.ColorOptionConfig()
+			{
+				// I have no clue how this works, so I hope that it does in fact work.
+				checkIfDisabled = new RiskOfOptions.OptionConfigs.BaseOptionConfig.IsDisabledDelegate(inputCheckIfDisabled)
+			};
+			RiskOfOptions.ModSettingsManager.AddOption(new RiskOfOptions.Options.ColorOption(newEntry, OptionConfig));
+		}
 
-        //public static RiskOfOptions.Options.CheckBoxOption CreateCheckBoxOption(ConfigEntry<bool> newEntry)
-        //{
-        //	return new RiskOfOptions.Options.CheckBoxOption(newEntry);
-        //}
+		//public static RiskOfOptions.Options.CheckBoxOption CreateCheckBoxOption(ConfigEntry<bool> newEntry)
+		//{
+		//	return new RiskOfOptions.Options.CheckBoxOption(newEntry);
+		//}
 
-        //public static void InvokeAddOption(RiskOfOptions.Options.BaseOption option)
-        //{
-        //	RiskOfOptions.ModSettingsManager.AddOption(option);
-        //}
+		//public static void InvokeAddOption(RiskOfOptions.Options.BaseOption option)
+		//{
+		//	RiskOfOptions.ModSettingsManager.AddOption(option);
+		//}
 
-        public static void InvokeSetModDescription(string description)
+		public static void InvokeSetModDescription(string description)
 		{
 			RiskOfOptions.ModSettingsManager.SetModDescription(description);
 		}
